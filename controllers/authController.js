e81b385fe3f2e07f788ea444a37f2b7a2a2fc569
patoary <zahidsagar93@gmail.com>
2024-02-02 22:57:08 +0900

    implement jwt and cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 // external imports
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 // internal imports
 const User = require("../models/People");
 const createError = require("http-errors");
@@ -30,7 +31,27 @@ const login = async (req, res, next) => {
     );
     if (!isPasswordCorrect) throw createError("Login failed! Please try again");
 
-    res.status(200).send(user);
+    //generate token
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: process.env.JWT_SECRET_EXPIRY,
+      }
+    );
+    //set coolie for batter security
+
+    // separate user info and don't send in client side
+    const { password, isAdmin, mobile, ...otherDetails } = user._doc;
+    res
+      // set cookie for security
+      .cookie(process.env.COOKIE_NAME, token, {
+        maxAge: process.env.JWT_SECRET_EXPIRY,
+        httpOnly: true,
+        signed: true,
+      })
+      .status(200)
+      .send({ ...otherDetails });
   } catch (err) {
     next(err);
   }
--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const port = process.env.PORT || 4000;
 dotenv.config();
-
-app.use(cors());
-app.use(express.json());
 
 // internal imports
 const hotelRouter = require("./router/hotelRouter");
@@ -28,6 +26,18 @@ mongoose
 mongoose.connection.on("error", (err) => {
   logError(err);
 });
+//
+app.use(cors());
+// request parsers
+/*
+
+In an Express.js application, the app.use(express.json()) middleware is used to parse incoming requests with JSON payloads. When a client sends data to the server, it's often sent in the body of the HTTP request, especially in the case of POST, PUT, and PATCH requests. This data is typically in JSON format.
+The express.json() middleware parses the incoming JSON payload and makes it available in the request.body object. This allows you to easily work with the data sent by the client in your route handlers.
+*/
+app.use(express.json());
+
+// cookie parser
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // router setup
 app.use("/hotel", hotelRouter);
